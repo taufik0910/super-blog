d3ee9b44b90f086525a4c4a904e56ac8ce22cf16
taufik0910 <taufik.cradle@gmail.com>
2020-04-05 14:53:08 +0700

    install vue router

--- resources/views/admin/masteradmin.blade.php
+++ resources/views/admin/masteradmin.blade.php
@@ -4,15 +4,16 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>AdminLTE 3 | Blank Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini" >
 <!-- Site wrapper -->
-<div class="wrapper">
+<div class="wrapper" id="app">
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -148,7 +149,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.0.4
     </div>
-    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">taufik learn Fullstack web</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 <a href="https://github.com/taufik0910">taufik learn Fullstack web</a>.</strong> All rights
     reserved.
   </footer>
 
