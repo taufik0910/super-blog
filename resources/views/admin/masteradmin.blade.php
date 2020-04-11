95860bc52a7cbe8e5b1c070a4b1b80df70e083bc
taufik0910 <taufik.cradle@gmail.com>
2020-04-11 19:19:33 +0700

    revisi footer

--- resources/views/admin/masteradmin.blade.php
+++ resources/views/admin/masteradmin.blade.php
@@ -100,7 +100,7 @@
                 <div class="float-right d-none d-sm-block">
                     <b>Version</b> 1.0
                 </div>
-                <strong>Copyright &copy; 2014-2018 <a href="http://sumon-it.com">FullStack Web</a>.</strong> All rights
+                <strong>Copyright &copy; 2020 <a href="https://github.com/taufik0910">FullStack Web</a>.</strong> All rights
                 reserved.
             </footer>
             
